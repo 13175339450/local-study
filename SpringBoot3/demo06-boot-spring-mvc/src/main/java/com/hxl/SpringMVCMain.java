383fad068b4f211f4e17e41be5568f8cff791ec0
hengxiaoliang <hengxl@vvmeeting.com>
2025-10-09 14:30:21 +0800

    [2025/10/9]springMVC结构

---
+++ SpringBoot3/demo06-boot-spring-mvc/src/main/java/com/hxl/SpringMVCMain.java
@@ -0,0 +1,12 @@
+package com.hxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVCMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMVCMain.class, args);
+    }
+}
