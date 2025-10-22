0dc85102689ed479585b0068a1aee0db53181966
hxl <hxl@git.com>
2025-10-22 23:17:18 +0800

    [2025/10/22]全注解开发之配置类准备

---
+++ SpringMvc/demo09-all-annotation-dev/src/main/java/com/hxl/config/SpringMvcConfig.java
@@ -0,0 +1,10 @@
+package com.hxl.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringMVC配置类
+ */
+@Configuration
+public class SpringMvcConfig {
+}
---
+++ SpringMvc/demo09-all-annotation-dev/src/main/java/com/hxl/config/WebMvcConfig.java
@@ -0,0 +1,51 @@
+package com.hxl.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class WebMvcConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    /**
+     * 配置Spring
+     */
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    /**
+     * 配置SpringMVC
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    /**
+     * 配置映射路径
+     */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    /**
+     * 额外配置过滤器
+     */
+    @Override
+    protected Filter[] getServletFilters() {
+        // 1.配置字符编码过滤器
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceRequestEncoding(true);
+        encodingFilter.setForceResponseEncoding(true);
+
+        // 2.配置HiddenHttpMethodFilter
+        HiddenHttpMethodFilter httpMethodFilter = new HiddenHttpMethodFilter();
+        return new Filter[]{encodingFilter, httpMethodFilter};
+    }
+}
