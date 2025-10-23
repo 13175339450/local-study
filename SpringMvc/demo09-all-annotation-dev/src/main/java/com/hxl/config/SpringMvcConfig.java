c456e62c824e59f05dc4be082da9ebdcf09fc9b5
hxl <hxl@git.com>
2025-10-23 23:03:26 +0800

    [2025/10/23]全注解开发之配置视图控制器和异常处理器

--- SpringMvc/demo09-all-annotation-dev/src/main/java/com/hxl/config/SpringMvcConfig.java
+++ SpringMvc/demo09-all-annotation-dev/src/main/java/com/hxl/config/SpringMvcConfig.java
@@ -4,14 +4,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.List;
+import java.util.Properties;
 
 /**
  * SpringMVC配置类
@@ -55,5 +61,33 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    /**
+     * 视图控制器
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("first");
+    }
+
+    /**
+     * 配置异常处理器
+     */
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        // 可以配置多个异常处理器
+        SimpleMappingExceptionResolver resolver = new SimpleMappingExceptionResolver();
+
+        // 设置其中的 exceptionMappings 属性
+        Properties pro = new Properties();
+        pro.setProperty("java.lang.Exception", "tip");
+        resolver.setExceptionMappings(pro);
+
+        // 设置其中的 exceptionAttribute 属性
+        resolver.setExceptionAttribute("e");
+
+        // 添加异常处理器
+        resolvers.add(resolver);
     }
 }
