51e92cf12716a323a8d9b2bd97ad9a5c46bcda0b
hxl <hxl@git.com>
2025-10-26 20:01:20 +0800

    [2025/10/26]pom继承修复

---
+++ SpringBoot3/demo08-mvc-servlet-tomcat/src/main/java/com/hxl/ServletMain.java
@@ -0,0 +1,11 @@
+package com.hxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ServletMain.class, args);
+    }
+}
