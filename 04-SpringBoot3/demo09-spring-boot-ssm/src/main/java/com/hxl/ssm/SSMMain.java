c88080c1b681103a020ccddfb970c047b1bee7ed
hxl <hxl@git.com>
2025-10-28 21:41:41 +0800

    [2025/10/28]SpringApplication简要分析

--- 04-SpringBoot3/demo09-spring-boot-ssm/src/main/java/com/hxl/ssm/SSMMain.java
+++ 04-SpringBoot3/demo09-spring-boot-ssm/src/main/java/com/hxl/ssm/SSMMain.java
@@ -1,8 +1,10 @@
 package com.hxl.ssm;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * 要扫描的Mapper.java类所在的文件
@@ -11,7 +13,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SSMMain {
 
+    /**
+     * 常规方式
+     */
+//    public static void main(String[] args) {
+    // SpringApplication: Boot应用的核心API入口
+//        SpringApplication.run(SSMMain.class, args);
+//    }
+
+    /**
+     * 分解方式
+     */
+//    public static void main(String[] args) {
+//        // 自定义SpringApplication的底层设置
+//        SpringApplication springApplication = new SpringApplication(SSMMain.class);
+//
+//        // 程序化调整 SpringApplication 的参数
+//        // springApplication.setDefaultProperties();
+//        //【配置文件优先级高于程序化调整优先级】
+//        springApplication.setBannerMode(Banner.Mode.OFF);
+//
+//        // SpringApplication 运行起来
+//        springApplication.run(args);
+//    }
+
+    /**
+     * 流式建造者模式
+     */
     public static void main(String[] args) {
-        SpringApplication.run(SSMMain.class, args);
+        new SpringApplicationBuilder()
+                .sources(SSMMain.class)
+                .bannerMode(Banner.Mode.CONSOLE)
+                .run(args);
     }
 }
