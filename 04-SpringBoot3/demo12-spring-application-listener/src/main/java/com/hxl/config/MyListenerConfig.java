d068cd98da47b6db29d86f3b4252e8b19df9a72f
hxl <hxl@git.com>
2025-10-29 23:26:31 +0800

    [2025/10/29]boot-listener-first

---
+++ 04-SpringBoot3/demo12-spring-application-listener/src/main/java/com/hxl/config/MyListenerConfig.java
@@ -0,0 +1,48 @@
+package com.hxl.config;
+
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.time.Duration;
+
+@Configuration
+public class MyListenerConfig implements SpringApplicationRunListener {
+
+    @Override
+    public void starting(ConfigurableBootstrapContext bootstrapContext) {
+        System.out.println("=================  boot启动时 ===================");
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext, ConfigurableEnvironment environment) {
+        System.out.println("=================  环境准备完成 ===================");
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+        System.out.println("=================  容器准备完成 ===================");
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+        System.out.println("=================  容器加载完成 ===================");
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context, Duration timeTaken) {
+        System.out.println("=================  boot启动完成 ===================");
+    }
+
+    @Override
+    public void ready(ConfigurableApplicationContext context, Duration timeTaken) {
+        System.out.println("=================  准备就绪 ===================");
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+        System.out.println("=================  启动失败 ===================");
+    }
+}
