efa0d92c2fb63bb3f26506e14ce7a97ee406cb88
hxl <hxl@git.com>
2025-11-01 23:04:45 +0800

    [2025/11/1]boot核心源码分析

---
+++ 04-SpringBoot3/demo14-boot-auto-configuration/src/main/java/com/hxl/auto/configuraion/BootAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.hxl.auto.configuraion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAutoConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(BootAutoConfiguration.class, args);
+    }
+}
